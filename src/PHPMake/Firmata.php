ad1eb33392eeebe3f339a578c51f5a020ce47c29
Keng-ichi Ahagon <k.ahagon@n-3.so>
2014-01-12 02:26:40 +0900

    add capability code

--- src/PHPMake/Firmata.php
+++ src/PHPMake/Firmata.php
@@ -7,4 +7,6 @@ class Firmata {
     const SYSEX_END = 0xF7;
     const QUERY_FIRMWARE = 0x79;
     const REPORT_VERSION = 0xF9;
+    const QUERY_CAPABILITY = 0x6B;
+    const RESPONSE_CAPABILITY = 0x6C;
 }
