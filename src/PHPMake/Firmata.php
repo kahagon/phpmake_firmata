86a8f287320d69d118753116b8de4d80f38678f9
Keng-ichi Ahagon <k.ahagon@n-3.so>
2014-03-05 02:14:30 +0900

    add capability codes

--- src/PHPMake/Firmata.php
+++ src/PHPMake/Firmata.php
@@ -10,4 +10,13 @@ class Firmata {
     const REPORT_VERSION = 0xF9;
     const QUERY_CAPABILITY = 0x6B;
     const RESPONSE_CAPABILITY = 0x6C;
+
+    const INPUT = 0x00;
+    const OUTPUT = 0x01;
+    const ANALOG = 0x02;
+    const PWM = 0x03;
+    const SERVO = 0x04;
+    const SHIFT = 0x05;
+    const I2C = 0x06;
+    
 }
