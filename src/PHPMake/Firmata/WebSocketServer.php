a059456de191c3dc8a6f17f11112ac76e61537d1
Keng-ichi Ahagon <k.ahagon@n-3.so>
2014-04-20 01:26:17 +0900

    integrate WebSocket server

---
+++ src/PHPMake/Firmata/WebSocketServer.php
@@ -0,0 +1,38 @@
+<?php
+namespace PHPMake\Firmata;
+use PHPMake\Firmata;
+use PHPMake\Firmata\WebSocketServer\ConnectionHub;
+use PHPMake\Firmata\WebSocketServer\ComponentInterface;
+
+class WebSocketServer {
+    private $_component;
+    private $_device;
+    private $_loop;
+    private $_server;
+
+    public function __construct(ComponentInterface $component, $port = 80, $address = '0.0.0.0') {
+        $this->_component = $component;
+        $this->_device = $this->_component->getDevice();
+        $this->_initLoop();
+        $this->_initServer($port, $address);
+    }
+
+    private function _initLoop() {
+        $this->_loop = new \React\EventLoop\StreamSelectLoop();
+        $this->_loop->addPeriodicTimer($this->_component->getInterval()/1000000, function () {
+            $this->_component->tick($this->_device);
+            $this->_device->noop();
+        });
+    }
+
+    private function _initServer($port, $address) {
+        $socket = new \React\Socket\Server($this->_loop);
+        $socket->listen($port, $address);
+        $mci = new \Ratchet\Http\HttpServer(new \Ratchet\WebSocket\WsServer($this->_component));
+        $this->_server = new \Ratchet\Server\IoServer($mci, $socket, $this->_loop);
+    }
+
+    public function run() {
+        $this->_server->run();
+    }
+}
